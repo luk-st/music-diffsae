d5a0f79c0c90c3ec6058b3dd0205b42b81b5ed00
lukasz-staniszewski <lukaszstaniszewski10@gmail.com>
2025-06-08 02:40:31 +0200

    feat: stable audio reconstruct hook

--- src/hooked_model/hooks.py
+++ src/hooked_model/hooks.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import einops
 
 
 class SAEReconstructHook:
@@ -55,3 +56,37 @@ class AblateHook:
         if isinstance(input, tuple):
             return (input[0],)
         return input[0]
+
+class StableAudioSAEReconstructHook:
+    def __init__(
+        self,
+        sae,
+        along_freqs=False
+    ):
+        self.sae = sae
+        self.along_freqs = along_freqs
+
+    @torch.no_grad()
+    def __call__(self, module, input, output):
+        output1, output2 = output.chunk(2)
+        batch_size = output1.shape[0]
+        if self.along_freqs:
+            output2 = einops.rearrange(output2, "b t f -> b f t")
+        # inside preprocess_input: x = x.reshape(batch_size * sample_size, emb_size)
+        sae_input, _, _ = self.sae.preprocess_input(output2)
+        pre_acts = self.sae.pre_acts(sae_input)
+        top_acts, top_indices = self.sae.select_topk(pre_acts)
+        buf = top_acts.new_zeros(top_acts.shape[:-1] + (self.sae.W_dec.mT.shape[-1],))
+        latents = buf.scatter_(dim=-1, index=top_indices, src=top_acts)
+        sae_out = (latents @ self.sae.W_dec) + self.sae.b_dec
+        if self.along_freqs:
+            sae_out = einops.rearrange(sae_out, "(b f) t -> b t f", b=batch_size)
+        hook_output = torch.cat(
+            [
+                output1,
+                sae_out
+            ],
+            dim=0,
+        )
+
+        return hook_output
